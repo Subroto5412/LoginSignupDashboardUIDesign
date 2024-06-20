5d9859f6606c0c974a2622571ecec356882b92c0
subroto <subroto_mohonto@yahoo.com>
2024-06-21 00:58:11 +0600

    search item

--- app/src/main/java/com/bd/loginsignupdashboarduidesign/MainActivity.kt
+++ app/src/main/java/com/bd/loginsignupdashboarduidesign/MainActivity.kt
@@ -15,12 +15,20 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
@@ -42,6 +50,7 @@ class MainActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Preview
     @Composable
     private fun dashboard()
@@ -69,20 +78,23 @@ class MainActivity : ComponentActivity() {
                         ), shape = RoundedCornerShape(bottomEnd = 40.dp, bottomStart = 40.dp)
                     )
                 )
-                Row(modifier = Modifier
-                    .padding(top=48.dp, start = 24.dp,end = 24.dp)
-                    .fillMaxWidth()
+                Row(
+                    modifier = Modifier
+                        .padding(top = 48.dp, start = 24.dp, end = 24.dp)
+                        .fillMaxWidth()
                 ) {
-                    Column(modifier = Modifier
-                        .height(100.dp)
-                        .padding(start = 14.dp)
-                        .weight(.7f),
+                    Column(
+                        modifier = Modifier
+                            .height(100.dp)
+                            .padding(start = 14.dp)
+                            .weight(.7f),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.Start
                     )
                     {
                         Text(text = "Hello", color = Color.White, fontSize = 18.sp)
-                        Text(text = "David Friedman",
+                        Text(
+                            text = "David Friedman",
                             color = Color.White,
                             fontSize = 22.sp,
                             fontWeight = FontWeight.Bold,
@@ -95,7 +107,7 @@ class MainActivity : ComponentActivity() {
                         modifier = Modifier
                             .width(100.dp)
                             .height(100.dp)
-                            .clickable {  }
+                            .clickable { }
                     )
                 }
 
@@ -116,68 +128,110 @@ class MainActivity : ComponentActivity() {
                         }
                 )
                 {
-                    Column(modifier = Modifier
-                        .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
-                        .height(90.dp)
-                        .width(90.dp)
-                        .background(color = Color(android.graphics.Color.parseColor("#ffe0c8")),
-                            shape = RoundedCornerShape(20.dp)
-                        ),
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Image(painter = painterResource(id = R.drawable.video_call),
+                    Column(
+                        modifier = Modifier
+                            .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
+                            .height(90.dp)
+                            .width(90.dp)
+                            .background(
+                                color = Color(android.graphics.Color.parseColor("#ffe0c8")),
+                                shape = RoundedCornerShape(20.dp)
+                            ),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.video_call),
                             contentDescription = null,
                             Modifier.padding(top = 8.dp, bottom = 4.dp)
                         )
-                        Text(text = "Video Call",
+                        Text(
+                            text = "Video Call",
                             fontSize = 14.sp,
                             fontWeight = FontWeight.Bold,
                             fontStyle = FontStyle.Italic,
-                            color = Color(android.graphics.Color.parseColor("#c77710")))
+                            color = Color(android.graphics.Color.parseColor("#c77710"))
+                        )
 
                     }
 
-                    Column(modifier = Modifier
-                        .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
-                        .height(90.dp)
-                        .width(90.dp)
-                        .background(color = Color(android.graphics.Color.parseColor("#ffe0c8")),
-                            shape = RoundedCornerShape(20.dp)
-                        ),
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Image(painter = painterResource(id = R.drawable.notification),
+                    Column(
+                        modifier = Modifier
+                            .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
+                            .height(90.dp)
+                            .width(90.dp)
+                            .background(
+                                color = Color(android.graphics.Color.parseColor("#ffe0c8")),
+                                shape = RoundedCornerShape(20.dp)
+                            ),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.notification),
                             contentDescription = null,
                             Modifier.padding(top = 8.dp, bottom = 4.dp)
                         )
-                        Text(text = "Notification",
+                        Text(
+                            text = "Notification",
                             fontSize = 14.sp,
                             fontWeight = FontWeight.Bold,
                             fontStyle = FontStyle.Italic,
-                            color = Color(android.graphics.Color.parseColor("#c77710")))
+                            color = Color(android.graphics.Color.parseColor("#c77710"))
+                        )
 
                     }
 
-                    Column(modifier = Modifier
-                        .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
-                        .height(90.dp)
-                        .width(90.dp)
-                        .background(color = Color(android.graphics.Color.parseColor("#ffe0c8")),
-                            shape = RoundedCornerShape(20.dp)
-                        ),
-                        horizontalAlignment = Alignment.CenterHorizontally) {
-                        Image(painter = painterResource(id = R.drawable.voice_call),
+                    Column(
+                        modifier = Modifier
+                            .padding(top = 12.dp, bottom = 12.dp, end = 12.dp)
+                            .height(90.dp)
+                            .width(90.dp)
+                            .background(
+                                color = Color(android.graphics.Color.parseColor("#ffe0c8")),
+                                shape = RoundedCornerShape(20.dp)
+                            ),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.voice_call),
                             contentDescription = null,
                             Modifier.padding(top = 8.dp, bottom = 4.dp)
                         )
-                        Text(text = "Voice Call",
+                        Text(
+                            text = "Voice Call",
                             fontSize = 14.sp,
                             fontWeight = FontWeight.Bold,
                             fontStyle = FontStyle.Italic,
-                            color = Color(android.graphics.Color.parseColor("#c77710")))
+                            color = Color(android.graphics.Color.parseColor("#c77710"))
+                        )
 
                     }
                 }
-
             }
+
+                var text by rememberSaveable { mutableStateOf("")}
+
+                TextField(value = text,
+                    onValueChange = { text = it },
+                    label = { Text(text = "Search for ...") },
+                    trailingIcon = {
+                        Image(
+                            painter = painterResource(R.drawable.search_icon),
+                            contentDescription = null,
+                            modifier = Modifier.size(43.dp).padding(end = 6.dp)
+                        )
+                    },
+                    shape = RoundedCornerShape(50.dp),
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color.Transparent,
+                        unfocusedBorderColor = Color.Transparent,
+                        unfocusedLabelColor = Color(android.graphics.Color.parseColor("#5e5e5e"))
+                    ),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 24.dp, end = 24.dp, start = 24.dp)
+                        .shadow(3.dp, shape = RoundedCornerShape(50.dp))
+                        .background(Color.White, CircleShape)
+                    )
         }
     }
 }
