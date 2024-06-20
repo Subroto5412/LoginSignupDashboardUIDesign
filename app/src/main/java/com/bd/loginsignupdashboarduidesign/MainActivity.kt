b17669a457728bdd3472bec67ea307deceff07dd
subroto <subroto_mohonto@yahoo.com>
2024-06-20 22:22:30 +0600

    dependencies added

--- app/src/main/java/com/bd/loginsignupdashboarduidesign/MainActivity.kt
+++ app/src/main/java/com/bd/loginsignupdashboarduidesign/MainActivity.kt
@@ -4,44 +4,77 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.bd.loginsignupdashboarduidesign.ui.theme.LoginSignupDashboardUIDesignTheme
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LoginSignupDashboardUIDesignTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            dashboard()
         }
     }
 }
 
+@OptIn(ExperimentalComposeUiApi::class)
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private fun dashboard() {
+    Column(
+        Modifier.fillMaxHeight().fillMaxWidth()
+            .background(color = Color(android.graphics.Color.parseColor("#f8eeec"))),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        ConstraintLayout {
+            val (topImg, profile) = createRefs()
+            Box(Modifier
+                .fillMaxWidth().height(245.dp)
+                .constrainAs(topImg) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                }
+                .background(
+                    brush = Brush.horizontalGradient(
+                        colors = listOf(
+                            Color(android.graphics.Color.parseColor("#EA6D35")),
+                            Color(android.graphics.Color.parseColor("#3b608c"))
+                        )
+                    ), shape = RoundedCornerShape(bottomEnd = 40.dp, bottomStart = 40.dp)
+                )
+            )
+            Row(modifier = Modifier
+                .padding(top=48.dp, start = 24.dp,end = 24.dp)
+                .fillMaxWidth()){
+                Column(modifier = Modifier
+                    .height(100.dp)
+                    .padding(start = 14.dp)
+                    .weight(.7f),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.Start
+                    ){
+                    Text(text = "Hello")
+                }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LoginSignupDashboardUIDesignTheme {
-        Greeting("Android")
+            }
+        }
     }
 }
